ae2bbe5f7069031cc0b1fdd91b4855f62a2e4541
omega202500 <jsimo8388@gmail.com>
2026-05-27 09:47:25 +0100

    pousser toutes les modifications

--- app/models/cassation.py
+++ app/models/cassation.py
@@ -3,12 +3,13 @@ from sqlalchemy.types import Numeric
 from app.database import Base
 from app.models.enums import TypeCassation, StatutCassation
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class Cassation(Base):
     __tablename__ = "cassations"
 
-    id                 = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id                 = Column(UUID(as_uuid=True), primary_key=True, default=lambda: uuid.uuid4())
     tontine_id         = Column(String(36), ForeignKey("tontines.id"), nullable=False)
     lot_id             = Column(String(36), ForeignKey("lots.id"), nullable=True)
     type_cassation     = Column(SAEnum(TypeCassation), nullable=False)

e970d24c670df1bf98e2e23996ca7d8243dcd337
omega202500 <jsimo8388@gmail.com>
2026-05-27 10:07:15 +0100

    pousser toutes les modifications

--- app/models/membre.py
+++ app/models/membre.py
@@ -2,6 +2,7 @@ from click import UUID
 from sqlalchemy import Column, String, Boolean, DateTime, Integer, Enum as SAEnum
 from app.database import Base
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
 import enum
 
@@ -15,7 +16,7 @@ class StatutMembre(str, enum.Enum):
 class Membre(Base):
     __tablename__ = "membres"
 
-    id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True),primary_key=True, default=uuid.uuid4)
     nom           = Column(String(100), nullable=False)
     prenom        = Column(String(100), nullable=False)
     age           = Column(Integer, nullable=True)
